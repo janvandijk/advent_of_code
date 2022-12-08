7e230ab6bd0d8cddcc878b50fbfdddfcda23d795
Jan van Dijk <iwritecode@gmail.com>
2022-12-08 09:06:43 +0100

    Day 8 opgeschoond

--- 2022/day8/day8.py
+++ 2022/day8/day8.py
@@ -1,25 +1,17 @@
 import numpy as np
 
 forest_input = open("input.txt", "r").read().splitlines()
-
 w, h =  len(forest_input[0]), len(forest_input)
-
 forest = np.empty((w,h), dtype = int)
-
 for x in range(0, w):
     for y in range(0, h):
         forest[x][y] = forest_input[x][y]
 
-print(forest)
-
 highest_trees = np.zeros((w,h), dtype = int)
-
 for x in range(0, w):
     start, finish = -1, -1
     for y in range(0, h):
-#        print("start {} - tree {}".format(start,forest[x][y]))
         if start < forest[x][y]:
-#            print("bingo")
             highest_trees[x][y] = 1
             start = forest[x][y]
         if finish < forest[x][h - y - 1]:
@@ -29,9 +21,7 @@ for x in range(0, w):
 for y in range(0, h):
     start, finish = -1, -1
     for x in range(0, w):
-#        print("start {} - tree {}".format(start,forest[x][y]))
         if start < forest[x][y]:
-#            print("bingo")
             highest_trees[x][y] = 1
             start = forest[x][y]
         if finish < forest[w - x - 1][y]:
@@ -41,101 +31,36 @@ for y in range(0, h):
 print("Aantal hoogste bomen: {}".format(sum(highest_trees.flatten())))
 
 score = 0
-
 for x in range(0, w):
     for y in range(0, h):
         height = forest[x][y]
         left,right,top,bottom = 0,0,0,0
 
-        print(height)
-
         # search left
         for i in range(x-1,-1,-1):
             left += 1
-            print("({},{}) = {}".format(i,y, forest[i][y]))
             if forest[i][y] >= height:
-                print("break")
                 break
-
-        print("done")
 
         # search right
         for i in range(x+1,w):
             right += 1
-            print("({},{}) = {}".format(i,y, forest[i][y]))
             if forest[i][y] >= height:
-                print("break")
                 break
-        print("done")
 
         # search top
         for i in range(y-1,-1,-1):
             top += 1
-            print("({},{}) = {}".format(x,i, forest[x][i]))
             if forest[x][i] >= height:
-                print("break")
                 break
-        print("done")
 
         # search bottom
         for i in range(y+1,h):
             bottom += 1
-            print("({},{}) = {}".format(x,i, forest[x][i]))
             if forest[x][i] >= height:
-                print("break")
                 break            
-        print("done")
 
         if left * right * top * bottom > score:
             score = left * right * top * bottom
 
-        print("({},{})={}: {} x {} x {} x {} = {}".format(x,y,forest[x][y], left,right,top,bottom,left * right * top * bottom))
-
-print(score)
-
-# x = 3
-# y = 2
-
-# height = forest[x][y]
-# left,right,top,bottom = 0,0,0,0
-
-# print(height)
-
-# # search left
-# for i in range(x-1,-1,-1):
-#     left += 1
-#     print("({},{}) = {}".format(i,y, forest[i][y]))
-#     if forest[i][y] >= height:
-#         print("break")
-#         break
-
-# print("done")
-
-# # search right
-# for i in range(x+1,w):
-#     right += 1
-#     print("({},{}) = {}".format(i,y, forest[i][y]))
-#     if forest[i][y] >= height:
-#         print("break")
-#         break
-# print("done")
-
-# # search top
-# for i in range(y-1,-1,-1):
-#     top += 1
-#     print("({},{}) = {}".format(x,i, forest[x][i]))
-#     if forest[x][i] >= height:
-#         print("break")
-#         break
-# print("done")
-
-# # search bottom
-# for i in range(y+1,h):
-#     bottom += 1
-#     print("({},{}) = {}".format(x,i, forest[x][i]))
-#     if forest[x][i] >= height:
-#         print("break")
-#         break            
-# print("done")
-
-# print("({},{})={}: {} x {} x {} x {} = {}".format(x,y,forest[x][y], left,right,top,bottom,left * right * top * bottom))
+print("Best viewscore: {}".format(score))

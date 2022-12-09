0a62b6e82229d6263ad2907f7decff321d5e00b6
Jan van Dijk <iwritecode@gmail.com>
2022-12-09 12:22:58 +0100

    Optimize a bit

--- 2022/day9/day9.py
+++ 2022/day9/day9.py
@@ -1,30 +1,18 @@
 commands = open("input.txt", "r").read().splitlines()
 
-rope = []
 rope_length = 10
-all_seen = {(0,0)}
+rope = [(0,0) for i in range(rope_length)]
+all_seen = [{(0,0)} for i in range(rope_length)]
 
-for i in range(0,rope_length):
-    rope.append((0,0))
-
-def move(point,dir):
-    if dir == "R":
-        return (point[0] + 1, point[1])
-    if dir == "L":
-        return (point[0] -1, point[1])
-    if dir == "U":
-        return (point[0], point[1] + 1)
-    if dir == "D":
-        return (point[0], point[1] - 1)
-
+moves = {"R":(1,0), "U":(0,1), "L":(-1, 0), "D":(0,-1)}
+def move(point, dir):
+    return (point[0] + moves[dir][0], point[1] + moves[dir][1])
 
 for command in commands:
-    dir,length = command.split(" ")
+    dir, length = command.split(" ")
     for i in range(0, int(length)):
         rope[0] = move(rope[0], dir)
-        # move rest of the rope
         for i in range(1, rope_length):
-            # move diagonally?
             if abs(rope[i-1][0] - rope[i][0]) + abs(rope[i-1][1] - rope[i][1]) > 2:
                 if rope[i-1][0] - rope[i][0] < 0:
                     rope[i] = move(rope[i],"L")
@@ -43,8 +31,7 @@ for command in commands:
                     rope[i] = move(rope[i],"U")
                 if rope[i-1][1] - rope[i][1] < -1:
                     rope[i] = move(rope[i],"D")
-            if i == rope_length -1:
-                all_seen.add(rope[i])
+            all_seen[i].add(rope[i])
                     
-print(len(all_seen))
-
+print(len(all_seen[1]))
+print(len(all_seen[-1]))
